13c22cd42da5ed07b246d12f937ec193437e4840
Philipp Hausmann <philipp.hausmann@314.ch>
2012-05-24 16:28:57 +0200

    -Added more Atomic* classes

---
+++ src/Pi.Data/Concurrent/AtomicDouble.cs
@@ -0,0 +1,60 @@
+// 
+// AtomicDouble.cs
+//  
+// Author:
+//       Philipp Hausmann <philipp_code@314.ch>
+// 
+// Copyright (c) 2012 Philipp Hausmann
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Threading;
+
+namespace Pi.Data.Concurrent
+{
+	public class AtomicDouble
+	{
+		private double value;
+
+		public AtomicDouble(double initialValue)
+		{
+			Set(initialValue);
+		}
+
+		public double CompareExchange(double newValue, double comparand)
+		{
+			return Interlocked.CompareExchange(ref this.value, newValue, comparand);
+		}
+
+		public double Exchange(double newValue)
+		{
+			return Interlocked.Exchange(ref this.value, newValue);
+		}
+		
+		public double Get() {
+			return Thread.VolatileRead(ref value);
+		}
+		
+		public void Set(double newValue) {
+			Thread.VolatileWrite(ref this.value, newValue);
+		}
+
+	}
+}
+
